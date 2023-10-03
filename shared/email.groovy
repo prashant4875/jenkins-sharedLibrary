a1205e7c39134b31d95f2d62ea3370c509f7262a
Prashant <bhadoria998@gmail.com>
2023-10-03 19:18:49 +0530

    add

---
+++ shared/email.groovy
@@ -0,0 +1,11 @@
+def call(emailTo, emailFrom, displayName, result, jobName, buildNumber, buildUrl):
+    emailext (
+            to: emailTo,
+            from: emailFrom,
+            subject: "This is a build for ${displayName}:${result}",
+            body: """
+                JenkinsBuild: ${jobName}
+                BuildNumber: ${buildNumber}
+                BuildUrl: ${buildUrl}
+            """
+    )
