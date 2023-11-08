308a875ce81edeb4778a079cfe816182a8701a00
Prashant <bhadoria998@gmail.com>
2023-11-08 19:03:18 +0530

    add

--- vars/dev.groovy
+++ vars/dev.groovy
@@ -8,6 +8,8 @@ def call( Map propertyInfo ){
                     script{
                         echo "${params.Environment}"
                         echo "${params.GithubRepo}"
+                        git branch: 'develop', url: "${params.GithubRepo}"
+                        sh "git checkout develop"
                     }
                 }
             }
