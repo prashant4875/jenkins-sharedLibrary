04f047959c97f480c5eb6a43b16575be7503e164
Prashant <bhadoria998@gmail.com>
2023-12-04 12:42:22 +0530

    add

--- jenkinsfile-dev.groovy
+++ jenkinsfile-dev.groovy
@@ -9,7 +9,7 @@ properties([
             defaultValue: 'origin/master',
             sortMode: 'DESCENDING_SMART',
             type: 'PT_BRANCH',
-            // useRepository: 'https://github.com/prashant4875/Netflix-clone.git'
+            useRepository: 'https://github.com/prashant4875/Netflix-clone.git'
         )
     ])
 ])
